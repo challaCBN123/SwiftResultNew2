ecf17fe445733e9e81e0744f79a6f4f6b5367936
challaCBN123 <challabhargava55@gmail.com>
2020-08-02 16:00:20 +0530

    Initial Commit

---
+++ SwiftResultNew/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftResultNew
+//
+//  Created by Bhargava on 02/08/20.
+//  Copyright © 2020 Bhargava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
